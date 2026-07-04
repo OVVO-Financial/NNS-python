7b75c679afed3927d2b287e407a2efd552d719f7
OVVO-Financial <ovvo.financial.systems@gmail.com>
2026-07-04 14:13:32 -0400

    Bump version to 1.0.9

--- src/nns/__init__.py
+++ src/nns/__init__.py
@@ -4,7 +4,7 @@ from typing import Any
 
 from nns.pm_matrix import pm_matrix as pm_matrix
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 
 _EXPORTS = {
     "FactorDesign": ("nns.regression", "FactorDesign"),
